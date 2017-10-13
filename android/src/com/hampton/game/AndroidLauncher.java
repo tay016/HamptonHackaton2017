253886f876a78945344d246b30d9742a634996a4
trunerd <turnerd@google.com>
2017-10-13 13:16:15 -0400

    initial

---
+++ android/src/com/hampton/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hampton.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hampton.game.HamptonHack;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HamptonHack(), config);
+	}
+}
