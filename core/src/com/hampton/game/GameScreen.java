de0fd62194629b87e4e03fa4b8cccc6fa6031595
trunerd <turnerd@google.com>
2017-10-13 17:34:07 -0400

    added timing test

--- core/src/com/hampton/game/GameScreen.java
+++ core/src/com/hampton/game/GameScreen.java
@@ -8,7 +8,9 @@ public abstract class GameScreen {
     protected Stage stage;
     protected ActorUtils utils;
     private StateManager stateManager;
+    private long numFrames;
     protected Color backgroundColor = new Color();
+    private long customFrameAmount;
 
     public void setData(Stage stage, ActorUtils utils, StateManager stateManager) {
         this.stage = stage;
@@ -33,4 +35,37 @@ public abstract class GameScreen {
     public Color getBackgroundColor() {
         return backgroundColor;
     }
+
+    /**
+     * Called before actors act!
+     */
+    public final void calledDuringRender() {
+        calledEveryFrame();
+        numFrames++;
+        if (numFrames % 60 == 0) {
+            calledAboutEverySecond();
+        }
+        if (numFrames % customFrameAmount == 0) {
+            calledCustomAmount();
+        }
+    }
+
+    protected final void setCustomFrameTime(long timeInMillis) {
+        customFrameAmount = timeInMillis * 10 / 60;
+    }
+
+    /**
+     * Called before actors act!
+     */
+    public abstract void calledAboutEverySecond();
+
+    /**
+     * Called before actors act!
+     */
+    public abstract void calledEveryFrame();
+
+    /**
+     * Called before actors act!
+     */
+    public abstract void calledCustomAmount();
 }
